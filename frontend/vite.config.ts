751feecebe3f1cbe7a6e033191e8cb6ffd25182e
Athul Anoop <athulanoop2020@gmail.com>
2025-03-28 18:10:15 +0530

    fix: css not loading

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 // Remove incorrect import and use the proper way to configure tailwindcss
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 });
