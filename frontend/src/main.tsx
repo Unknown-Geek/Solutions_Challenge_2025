fb0ebd997281ec8c8462f445ec775945b7519890
Athul Anoop <athulanoop2020@gmail.com>
2025-03-28 17:49:02 +0530

    fix: typescript error

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-// @ts-ignore
 import "./index.css";
 import App from "./App";
 //import { ThemeProvider } from "./components/theme-provider"
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 
 root.render(
@@ -15,7 +14,7 @@ root.render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 /* <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
 <App />
